a5bed498ecb5c0ebf72ffedbd8dadccc7d1bbaa8
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 02:04:36 -0700

    Update feds.py

--- DewmiBot/modules/feds.py
+++ DewmiBot/modules/feds.py
@@ -2339,20 +2339,20 @@ def get_chat(chat_id, chat_data):
 @run_async
 def fed_owner_help(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        """*👑 Fed Owner Only:*
-👉  /newfed <fed_name>*:* Creates a Federation, One allowed per user
-👉  /renamefed <fed_id> <new_fed_name>*:* Renames the fed id to a new name
-👉  /delfed <fed_id`*:* Delete a Federation, and any information related to it. Will not cancel blocked users
-👉  /fpromote <user>*:* Assigns the user as a federation admin. Enables all commands for the user under `Fed Admins`
-👉  /fdemote <user>*:* Drops the User from the admin Federation to a normal User
-👉  /subfed <fed_id>*:* Subscribes to a given fed ID, bans from that subscribed fed will also happen in your fed
-👉  /unsubfed <fed_id>*:* Unsubscribes to a given fed ID
-👉  /setfedlog <fed_id>*:* Sets the group as a fed log report base for the federation
-👉  /unsetfedlog <fed_id>*:* Removed the group as a fed log report base for the federation
-👉  /fbroadcast <message>*:* Broadcasts a messages to all groups that have joined your fed
-👉  /fedsubs*:* Shows the feds your group is subscribed to `(broken rn)`
+        """*Fed Owner Only:*
+❍  /newfed <fed_name>*:* Creates a Federation, One allowed per user
+❍  /renamefed <fed_id> <new_fed_name>*:* Renames the fed id to a new name
+❍  /delfed <fed_id`*:* Delete a Federation, and any information related to it. Will not cancel blocked users
+❍  /fpromote <user>*:* Assigns the user as a federation admin. Enables all commands for the user under `Fed Admins`
+❍  /fdemote <user>*:* Drops the User from the admin Federation to a normal User
+❍  /subfed <fed_id>*:* Subscribes to a given fed ID, bans from that subscribed fed will also happen in your fed
+❍  /unsubfed <fed_id>*:* Unsubscribes to a given fed ID
+❍  /setfedlog <fed_id>*:* Sets the group as a fed log report base for the federation
+❍  /unsetfedlog <fed_id>*:* Removed the group as a fed log report base for the federation
+❍  /fbroadcast <message>*:* Broadcasts a messages to all groups that have joined your fed
+❍  /fedsubs*:* Shows the feds your group is subscribed to `(broken rn)`
 
-@dewmibot""",
+@szrosebot🇱🇰""",
         parse_mode=ParseMode.MARKDOWN,
     )
 
@@ -2360,19 +2360,19 @@ def fed_owner_help(update: Update, context: CallbackContext):
 @run_async
 def fed_admin_help(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        """*🔱 Fed Admins:*
-👉  fban <user> <reason>*:* Fed bans a user
-👉  /unfban <user> <reason>*:* Removes a user from a fed ban
-👉  /fedinfo <fed_id>*:* Information about the specified Federation
-👉  /joinfed <fed_id>*:* Join the current chat to the Federation. Only chat owners can do this. Every chat can only be in one Federation
-👉  /leavefed <fed_id>*:* Leave the Federation given. Only chat owners can do this
-👉  /setfrules <rules>*:* Arrange Federation rules
-👉  /fedadmins*:* Show Federation admin
-👉  /fbanlist*:* Displays all users who are victimized at the Federation at this time
-👉  /fedchats*:* Get all the chats that are connected in the Federation
-👉  /chatfed *:* See the Federation in the current chat
+        """* Fed Admins:*
+❍  fban <user> <reason>*:* Fed bans a user
+❍  /unfban <user> <reason>*:* Removes a user from a fed ban
+❍  /fedinfo <fed_id>*:* Information about the specified Federation
+❍  /joinfed <fed_id>*:* Join the current chat to the Federation. Only chat owners can do this. Every chat can only be in one Federation
+❍  /leavefed <fed_id>*:* Leave the Federation given. Only chat owners can do this
+❍  /setfrules <rules>*:* Arrange Federation rules
+❍  /fedadmins*:* Show Federation admin
+❍  /fbanlist*:* Displays all users who are victimized at the Federation at this time
+❍  /fedchats*:* Get all the chats that are connected in the Federation
+❍  /chatfed *:* See the Federation in the current chat
 
-@dewmibot\n""",
+@szrosebot🇱🇰\n""",
         parse_mode=ParseMode.MARKDOWN,
     )
 
@@ -2380,19 +2380,20 @@ def fed_admin_help(update: Update, context: CallbackContext):
 @run_async
 def fed_user_help(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
-        """*🎩 Any user:*
-👉  /fbanstat*:* Shows if you/or the user you are replying to or their username is fbanned somewhere or not
-👉  /fednotif <on/off>*:* Federation settings not in PM when there are users who are fbaned/unfbanned
-👉  /frules*:* See Federation regulations
+        """* Any user:*
+❍  /fbanstat*:* Shows if you/or the user you are replying to or their username is fbanned somewhere or not
+❍  /fednotif <on/off>*:* Federation settings not in PM when there are users who are fbaned/unfbanned
+❍  /frules*:* See Federation regulations
 
-@dewmibot\n""",
+@szrosebot🇱🇰\n""",
         parse_mode=ParseMode.MARKDOWN,
     )
 
 
-__mod_name__ = "Federations 👥"
+__mod_name__ = "Federations"
 
 __help__ = """
+@szrosebot🇱🇰
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
 *No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
@@ -2400,11 +2401,11 @@ You can even designate federation admins, so your trusted admin can ban all the 
 
 *Commands:*\n
 Feds are now divided into 3 sections for your ease. 
-👉 /fedownerhelp*:* Provides help for fed creation and owner only commands
-👉 /fedadminhelp*:* Provides help for fed administration commands
-👉 /feduserhelp*:* Provides help for commands anyone can use
+❍ /fedownerhelp*:* Provides help for fed creation and owner only commands
+❍ /fedadminhelp*:* Provides help for fed administration commands
+❍ /feduserhelp*:* Provides help for commands anyone can use
 
-@dewmibot
+
 """
 
 NEW_FED_HANDLER = CommandHandler("newfed", new_fed)
