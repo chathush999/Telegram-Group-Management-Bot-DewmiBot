28258c2e3e90dc47ca246792a139beb4e24cdc1a
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 01:44:53 -0700

    rose

--- DewmiBot/modules/cleaner.py
+++ DewmiBot/modules/cleaner.py
@@ -230,17 +230,17 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
 
 
 __help__ = """
+@szrosebot🇱🇰
 *Blue text cleaner removed any made up commands that people send in your chat.*
- 👉 /cleanblue <on/off/yes/no>*:* clean commands after sending
- 👉 /ignoreblue <word>*:* prevent auto cleaning of the command
- 👉 /unignoreblue <word>*:* remove prevent auto cleaning of the command
- 👉 /listblue*:* list currently whitelisted commands
+ ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
+ ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
+ ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
+ ❍ /listblue*:* list currently whitelisted commands
  
  *Following are Disasters only commands, admins cannot use these:*
- 👉 /gignoreblue <word>*:* globally ignores bluetext cleaning of saved word across Suzuya.
- 👉 /ungignoreblue <word>*:* remove said command from global cleaning list
+ ❍ /gignoreblue <word>*:* globally ignores bluetext cleaning of saved word across Suzuya.
+ ❍ /ungignoreblue <word>*:* remove said command from global cleaning list
  
- @dewmibot
 """
 
 SET_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("cleanblue", set_blue_text_must_click)
@@ -265,7 +265,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "Cleaning 🧹"
+__mod_name__ = "Cleaning"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
