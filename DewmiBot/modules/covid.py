cec7719b111515a5cfefe03c934572b828d45119
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 01:52:47 -0700

    Update covid.py

--- DewmiBot/modules/covid.py
+++ DewmiBot/modules/covid.py
@@ -12,11 +12,11 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Global Totals** 🦠\n😓Cases: {r['cases']:,}\n😓Cases Today: {r['todayCases']:,}\n😓Deaths: {r['deaths']:,}\n😓Deaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\n😓Active: {r['active']:,}\n😓Critical: {r['critical']:,}\n😓Cases/Mil: {r['casesPerOneMillion']}\n😓Deaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\n😓Cases Today: {r['todayCases']:,}\n😓Deaths: {r['deaths']:,}\n😓Deaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\n😓Active: {r['active']:,}\n😓Critical: {r['critical']:,}\n😓Cases/Mil: {r['casesPerOneMillion']}\n😓Deaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
