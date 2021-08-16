c884a7aff40fee19258a05859623b1933d296d31
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 02:11:47 -0700

    Update locks.py

--- DewmiBot/modules/locks.py
+++ DewmiBot/modules/locks.py
@@ -555,16 +555,17 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
+@szrosebot🇱🇰
 Do stickers annoy you? or want to avoid people sharing links? or pictures? \
 You're in the right place!
 The locks module allows you to lock away some common items in the \
 telegram world; the bot will automatically delete them!
- 👉 `/locktypes`*:* Lists all possible locktypes
+❍ `/locktypes`*:* Lists all possible locktypes
  
 *Admins only:*
- 👉 `/lock <type>`*:* Lock items of a certain type (not available in private)
- 👉 `/unlock <type>`*:* Unlock items of a certain type (not available in private)
- 👉 `/locks`*:* The current list of locks in this chat.
+ ❍ `/lock <type>`*:* Lock items of a certain type (not available in private)
+ ❍ `/unlock <type>`*:* Unlock items of a certain type (not available in private)
+ ❍ `/locks`*:* The current list of locks in this chat.
  
 Locks can be used to restrict a group's users.
 eg:
@@ -572,13 +573,12 @@ Locking urls will auto-delete all messages with urls, locking stickers will rest
 non-admin users from sending stickers, etc.
 Locking bots will stop non-admins from adding bots to the chat.
 *Note:*
- 👉 Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
- 👉 Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
+ ❍ Unlocking permission *info* will allow members (non-admins) to change the group information, such as the description or the group name
+ ❍ Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
  
- @dewmibot
 """
 
-__mod_name__ = "Locks 🔐"
+__mod_name__ = "Locks "
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
