90971add362c2f6f92cee6c6ed3d593a66b87d4f
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 01:56:08 -0700

    Update cust_filters.py

--- DewmiBot/modules/cust_filters.py
+++ DewmiBot/modules/cust_filters.py
@@ -607,10 +607,11 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- 👉 /filters*:* List all active filters saved in the chat.
+@szrosebot🇱🇰
+ ❍ /filters*:* List all active filters saved in the chat.
 
 *Admin only:*
- 👉 /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ ❍ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
@@ -622,18 +623,17 @@ doin?
  Reply 2
  %%%
  Reply 3`
- 👉 /stop <filter keyword>*:* Stop that filter.
+ ❍ /stop <filter keyword>*:* Stop that filter.
 
 *Chat creator only:*
- 👉 /removeallfilters*:* Remove all chat filters at once.
+ ❍ /removeallfilters*:* Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
 Check `/markdownhelp` to know more!
 
-@dewmibot
 """
 
-__mod_name__ = "Filters 📜"
+__mod_name__ = "Filters "
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
