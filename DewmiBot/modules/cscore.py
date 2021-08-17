e0a369fe4eeaa1add9034fdfb5de4aa76514a524
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 22:21:16 -0700

    Update cscore.py

--- DewmiBot/modules/cscore.py
+++ DewmiBot/modules/cscore.py
@@ -2,32 +2,21 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
+from DewmiBot import telethn as tbot
 from telethon.tl import functions, types
-
-from tele import tbot
+from telethon.tl.types import *
 
 
 async def is_register_admin(chat, user):
-
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
         return isinstance(
             (
                 await tbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
-        ).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
+    if isinstance(chat, types.InputPeerUser):
+        return True
 
 
 @tbot.on(events.NewMessage(pattern="/cs$"))
@@ -35,10 +24,10 @@ async def _(event):
     if event.fwd_from:
         return
     if event.is_group:
-        if await is_register_admin(event.input_chat, event.message.sender_id):
-            pass
-        else:
-            return
+     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("🚨 Need Admin Pewer.. You can't use this command.. But you can use in my pm")
+       return
+
     score_page = "http://static.cricinfo.com/rss/livescores.xml"
     page = urllib.request.urlopen(score_page)
     soup = BeautifulSoup(page, "html.parser")
