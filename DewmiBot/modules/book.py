449f6b7a46709e774838187b0081c645fa8b2b5e
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 22:48:06 -0700

    Update book.py

--- DewmiBot/modules/book.py
+++ DewmiBot/modules/book.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 from telethon import events
 
-from DewmiBot.telethon import tbot
+from DewmiBot import tbot
 
 
 @tbot.on(events.NewMessage(pattern="^/book (.*)"))
