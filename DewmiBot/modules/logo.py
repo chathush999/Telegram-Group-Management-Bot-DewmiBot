0337704a8880f00d6a39eeb8d8b3d655c621f90f
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 20:33:18 -0700

    Update logo.py

--- DewmiBot/modules/logo.py
+++ DewmiBot/modules/logo.py
@@ -90,7 +90,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 100)
+    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 50)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -105,44 +105,6 @@ async def lego(event):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
-
-@register(pattern="^/biglogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 950)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "AnkiVectorLogo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @szrosebot🇱🇰")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
-
 
 @register(pattern="^/wlogo ?(.*)")
 async def lego(event):
