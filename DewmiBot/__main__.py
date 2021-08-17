26993190902a78e51efa6a1b17465913de9cf670
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-17 02:34:55 -0700

    Update __main__.py

--- DewmiBot/__main__.py
+++ DewmiBot/__main__.py
@@ -238,12 +238,12 @@ def start(update: Update, context: CallbackContext):
             elif args[0][1:].isdigit() and "rules" in IMPORTED:
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
                 
-        else:       
-            update.effective_message.reply_text(   
+        else:
+            update.effective_message.reply_text(
                 PM_START_TEXT,
-                reply_markup=InlineKeyboardMarkup(buttons),
+                reply_markup=InlineKeyboardMarkup(BUTTONS),
                 parse_mode=ParseMode.MARKDOWN,
-                timeout=60,
+                timeout=60, 
             )
     else:
         update.effective_message.reply_text(
