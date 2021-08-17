abfefd498d5c2fda26f8af9482184e824fe08bcc
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-16 20:29:07 -0700

    Update logo.py

--- DewmiBot/modules/logo.py
+++ DewmiBot/modules/logo.py
@@ -90,7 +90,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 100)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
