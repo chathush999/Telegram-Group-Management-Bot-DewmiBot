df515dbe5d345c47962bc053585c92d3f58c46a8
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-18 21:28:32 -0700

    Update covid.py

--- DewmiBot/modules/covid.py
+++ DewmiBot/modules/covid.py
@@ -11,8 +11,8 @@ def covid(update: Update, context: CallbackContext):
     message = update.effective_message
     text = message.text.split(" ", 1)
     if len(text) == 1:
-        r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\n😓Cases: {r['cases']:,}\n😓Cases Today: {r['todayCases']:,}\n😓Deaths: {r['deaths']:,}\n😓Deaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\n😓Active: {r['active']:,}\n😓Critical: {r['critical']:,}\n😓Cases/Mil: {r['casesPerOneMillion']}\n😓Deaths/Mil: {r['deathsPerOneMillion']}"
+        r = requests.get("https://corona.lmao.ninja/v2/sri lanka").json()
+        reply_text = l: f"**🇱🇰 Local Covid 19 Stats  🇱🇰**🩺\n📊 Total Confirmed Cases: {r['cases']:,}\n😷  New Cases Today : {r['todayCases']:,}\n💥 Deaths: {r['deaths']:,}\n💣  New Deaths today : {r['todayDeaths']:,}\n📟  Recovered & Discharged: {r['recovered']:,}\n🔖 Active Cases: {r['active']:,}\n🏥 Individuals currently under investigations in hospitals: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
