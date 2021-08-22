fb31208712b4b9eda4108a5f3263002a2277db77
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-21 21:15:14 -0700

    Update vc.py

--- DewmiBot/modules/vc.py
+++ DewmiBot/modules/vc.py
@@ -1,42 +1,49 @@
 __help__ = """
 *  • Groups and channels Voice Chat Music Play 🎙 *
+
 1️⃣ Make bot admin (Group and in channel if use cplay)
 2️⃣ Start a voice chat
 3️⃣ Try /play [song name] for the first time by an admin
 4️⃣ If userbot joined enjoy music, If not add @vcpalyassistant to your group and retry
  
 * 🔔 For Channel Music Play 🔔 *
+
 1️⃣ Make @szrosebot  admin of your channel 
 2️⃣ Send /userbotjoinchannel in linked group
 3️⃣ Now send commands in linked group 
+
 * 👥Groups Music Play👥 *
-🔴 /play: Play song using youtube music
-🔴 /play [yt url] : Play the given yt url
-🔴 /play [reply yo audio]: Play replied audio
-🔴 /dplay: Play song via deezer
-🔴 /splay: Play song via jio saavn
+
+❍ /play: Play song using youtube music
+❍ /play [yt url] : Play the given yt url
+❍ /play [reply yo audio]: Play replied audio
+❍ /dplay: Play song via deezer
+❍ /splay: Play song via jio saavn
  
  * 🟣Playback🟣 *
-🔴 /player: Open Settings menu of player
-🔴 /skip: Skips the current track
-🔴 /pause: Pause track
-🔴 /resume: Resumes the paused track
-🔴 /end: Stops media playback
-🔴 /current: Shows the current Playing track
-🔴 /playlist: Shows playlist
+ 
+❍ /player: Open Settings menu of player
+❍ /skip: Skips the current track
+❍ /pause: Pause track
+❍ /resume: Resumes the paused track
+❍ /end: Stops media playback
+❍ /current: Shows the current Playing track
+❍ /playlist: Shows playlist
 Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group. 
+
 * 🔔Channel Music Play🔔 *
-⭕️ /cplay [song name] - play song you requested
-⭕️ /cdplay [song name] - play song you requested via deezer
-⭕️ /csplay [song name] - play song you requested via jio saavn
-⭕️ /cplaylist - Show now playing list
-⭕️ /cccurrent - Show now playing
-⭕️ /cplayer - open music player settings panel
-⭕️ /cpause - pause song play
-⭕️ /cresume - resume song play
-⭕️ /cskip - play next song
-⭕️ /cend - stop music play
-⭕️ /userbotjoinchannel - invite assistant to your chat
+
+❍ /cplay [song name] - play song you requested
+❍ /cdplay [song name] - play song you requested via deezer
+❍ /csplay [song name] - play song you requested via jio saavn
+❍ /cplaylist - Show now playing list
+❍ /cccurrent - Show now playing
+❍ /cplayer - open music player settings panel
+❍ /cpause - pause song play
+❍ /cresume - resume song play
+❍ /cskip - play next song
+❍ /cend - stop music play
+❍ /userbotjoinchannel - invite assistant to your chat
 channel is also can be used instead of c ( /cplay = /channelplay )
   
 *  If you donlt like to play in linked group  *
@@ -47,6 +54,7 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 5) Simply send commands in your group.
  
 * ⚫️More tools⚫️ *
+
 • /admincache: Updates admin info of your group. Try if bot isn't recognize admin
 • /userbotjoin: Invite @vcpalyassistant Userbot to your chat 
  
@@ -57,4 +65,4 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 
 @szrosebot
 """
-__mod_name__ = "VC Play "
+__mod_name__ = "VC music Play "
