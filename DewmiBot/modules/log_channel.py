4d5f9f2306695b2c31d1a6a9c9f77acb016e6d24
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-22 03:08:03 -0700

    Update log_channel.py

--- DewmiBot/modules/log_channel.py
+++ DewmiBot/modules/log_channel.py
@@ -206,7 +206,7 @@ if is_module_loaded(FILENAME):
  
 """
 
-    __mod_name__ = "Logger "
+    __mod_name__ = "Logger🗞 "
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
