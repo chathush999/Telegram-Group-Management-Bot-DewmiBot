bf9229f536aaac8c83540653aa74139b164c7cb3
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-21 23:21:31 -0700

    Update logos.py

--- DewmiBot/modules/logos.py
+++ DewmiBot/modules/logos.py
@@ -84,7 +84,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/77653060.png')
+    img = Image.open('./DewmiBot/resources/photo_2021-08-21_23-14-49-removebg-preview.png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
@@ -121,7 +121,7 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/77653060.png')
+    img = Image.open('./DewmiBot/resources/photo_2021-08-21_23-14-49-removebg-preview.png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
@@ -143,90 +143,7 @@ async def lego(event):
  except Exception as e:
    await event.reply(f'Join with us ~ @sl_bot_zone to use this, {e}')
 
-@register(pattern="^/daalogo ?(.*)")
-async def logo_gen(event):
-    xx = await eor(event, get_string("com_1"))
-    name = event.pattern_match.group(1)
-    if not name:
-        await eod(xx, "`Give a name too!`")
-    bg_, font_ = "", ""
-    if event.reply_to_msg_id:
-        temp = await event.get_reply_message()
-        if temp.media:
-            if hasattr(temp.media, "document"):
-                if "font" in temp.file.mime_type:
-                    font_ = await temp.download_media()
-                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
-                    font_ = await temp.download_media()
-            elif "pic" in mediainfo(temp.media):
-                bg_ = await temp.download_media()
-    else:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-        fpath_ = glob.glob("DewmiBot/resources/*")
-        font_ = random.choice(fpath_)
-    if not bg_:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-    if not font_:
-        fpath_ = glob.glob("DewmiBot/resources/*")
-        font_ = random.choice(fpath_)
-    if len(name) <= 8:
-        fnt_size = 150
-        strke = 10
-    elif len(name) >= 9:
-        fnt_size = 50
-        strke = 5
-    else:
-        fnt_size = 130
-        strke = 20
-    img = Image.open(bg_)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, fnt_size)
-    w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
-    image_width, image_height = img.size
-    draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
-        name,
-        font=font,
-        fill=(255, 255, 255),
-    )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text(
-        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
-    )
-    flnme = f"ultd.png"
-    img.save(flnme, "png")
-    await xx.edit("`Done!`")
-    if os.path.exists(flnme):
-        await event.client.send_file(
-            event.chat_id,
-            file=flnme,
-            caption=f"Logo by @szrosebot🇱🇰",
-            force_document=True,
-        )
-        os.remove(flnme)
-        await xx.delete()
-    if os.path.exists(bg_):
-        os.remove(bg_)
-    if os.path.exists(font_):
-        if not font_.startswith("DewmiBot/resources/"):
-            os.remove(font_)    
- 
-
-@register(pattern="^/redlogo ?(.*)")
+@register(pattern="^carbon ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
@@ -241,13 +158,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./DewmiBot/resources/77653060.png')
+    img = Image.open('./DewmiBot/resources/photo_2021-08-21_23-18-11.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./DewmiBot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./DewmiBot/resources/EraserDust.ttf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -310,8 +227,7 @@ __help__ = """
 @szrosebot🇱🇰
  ❍ /logo text :  Create your logo with your name
  ❍ /wlogo text :  Create your logo with your name
- ❍ /redlogo text :  Create your logo with your name
+ ❍ /carbon  text :  Create your logo with your name
  ❍ /pandalogo :  Create your logo with your name
- ❍ /daalogo :  Create your logo with your name
  """
 __mod_name__ = "Logo Maker"
