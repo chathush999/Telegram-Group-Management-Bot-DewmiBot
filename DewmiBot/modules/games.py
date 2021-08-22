2e3c3257eb3ca33efbbdcc48b19c0b385591bbe8
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-21 21:25:02 -0700

    Update games.py

--- DewmiBot/modules/games.py
+++ DewmiBot/modules/games.py
@@ -66,9 +66,9 @@ async def _(event):
 
 __help__ = """
  *Play Game With Emojis:*
-  - /dice or /dice 1 to 6 any value
-  - /ball or /ball 1 to 5 any value
-  - /dart or /dart 1 to 6 any value
+  ❍ /dice or /dice 1 to 6 any value
+  ❍ /ball or /ball 1 to 5 any value
+  ❍ /dart or /dart 1 to 6 any value
  Usage: hahaha just a magic.
  warning: you would be in trouble if you input any other value than mentioned.
 """
