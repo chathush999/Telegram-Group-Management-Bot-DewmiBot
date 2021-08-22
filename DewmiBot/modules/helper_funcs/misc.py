90ddbb7cac7d608a97432f3f4fc43c1cdb7b3f8a
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-21 21:00:44 -0700

    Update misc.py

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -1,9 +1,10 @@
 from math import ceil
-from typing import Dict, List
+from typing import List, Dict
 
-from DewmiBot import NO_LOAD
-from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
+from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode,Update
 from telegram.error import TelegramError
+
+from DewmiBot import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
@@ -21,20 +22,21 @@ def split_message(msg: str) -> List[str]:
     if len(msg) < MAX_MESSAGE_LENGTH:
         return [msg]
 
-    lines = msg.splitlines(True)
-    small_msg = ""
-    result = []
-    for line in lines:
-        if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
-            small_msg += line
-        else:
-            result.append(small_msg)
-            small_msg = line
     else:
-        # Else statement at the end of the for loop, so append the leftover string.
-        result.append(small_msg)
+        lines = msg.splitlines(True)
+        small_msg = ""
+        result = []
+        for line in lines:
+            if len(small_msg) + len(line) < MAX_MESSAGE_LENGTH:
+                small_msg += line
+            else:
+                result.append(small_msg)
+                small_msg = line
+        else:
+            # Else statement at the end of the for loop, so append the leftover string.
+            result.append(small_msg)
 
-    return result
+        return result
 
 
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
@@ -60,25 +62,25 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 10)
+    max_num_pages = ceil(len(pairs) / 6)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 10:
-        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️ Back ", callback_data="{}_prev({})".format(prefix, modulo_page)),
+    if len(pairs) > 6:
+        pairs = pairs[modulo_page * 6:6 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton(" Updates channel ", url="https://t.me/sl_bot_zone"),
-             EqInlineKeyboardButton("Next ➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton(" Updates channel ", url="https://t.me/sl_bot_zone")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
+
+
 
     return pairs
 
 
-def send_to_list(
-    bot: Bot, send_to: list, message: str, markdown=False, html=False
-) -> None:
+def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
     if html and markdown:
         raise Exception("Can only send with either markdown or HTML!")
     for user_id in set(send_to):
@@ -114,6 +116,10 @@ def revert_buttons(buttons):
 
     return res
 
+def sendMessage(text: str, bot: Bot, update: Update):
+    return bot.send_message(update.message.chat_id,
+                                    reply_to_message_id=update.message.message_id,
+                                    text=text, parse_mode=ParseMode.HTML)
 
 def build_keyboard_parser(bot, chat_id, buttons):
     keyb = []
@@ -127,6 +133,7 @@ def build_keyboard_parser(bot, chat_id, buttons):
 
     return keyb
 
-
 def is_module_loaded(name):
-    return name not in NO_LOAD
+     return name not in NO_LOAD
+                
+    
