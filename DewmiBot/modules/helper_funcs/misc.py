aa8ef130c6335c0a5d81925e49ca7f2ff2b5ba4c
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-23 18:49:18 -0700

    Update misc.py

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -1,4 +1,3 @@
-from math import ceil
 from typing import Dict, List
 
 from DewmiBot import NO_LOAD
@@ -49,22 +48,16 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                                     callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
              in module_dict.values()])
 
-    pairs = [
-    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
-    ]
+    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
 
-    round_num = len(modules) / 3
+    round_num = len(modules) / 4
     calc = len(modules) - round(round_num)
-    if calc == 1:
-        pairs.append((modules[-1], ))
+    if calc in [1, 2]:
+        pairs.append((modules[-1],))
     elif calc == 2:
-        pairs.append((modules[-1], ))
+        pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 10)
-    modulo_page = page_n % max_num_pages
-
-    # can only have a certain amount of buttons side by side
-    if len(pairs) > 10:
+    else:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("🔘 Main menu 🔘", callback_data="aboutmanu_"),
