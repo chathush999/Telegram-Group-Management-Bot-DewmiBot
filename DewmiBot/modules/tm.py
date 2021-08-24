4e03911b77a69a7442837553ce8ab4845992299a
githubsupun <88021479+githubsupun@users.noreply.github.com>
2021-08-23 21:17:20 -0700

    Update tm.py

--- DewmiBot/modules/tm.py
+++ DewmiBot/modules/tm.py
@@ -5,9 +5,9 @@ from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
-from telegraph import Telegraph, upload_file, exceptions
+from tm  import Telegraph, upload_file, exceptions
 Hero = "DewmiBot"
-telegraph = Telegraph()
+tm= Telegraph()
 r = telegraph.create_account(short_name=Hero)
 auth_url = r["auth_url"]
 
