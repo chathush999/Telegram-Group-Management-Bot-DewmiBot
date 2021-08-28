64e3ec21e75c78c8815a57fa7c8f6b3523ba49fa
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-28 01:17:01 -0700

    Update ytdl.py

--- DewmiBot/modules/ytdl.py
+++ DewmiBot/modules/ytdl.py
@@ -128,3 +128,10 @@ async def download_video(v_url):
             caption=ytdl_data["title"],
         )
         os.remove(f"{ytdl_data['id']}.mp4")
+        
+__help__ = """
+@szrosebot🇱🇰
+ ❍ /yt <text>: perform a youtube search
+ ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+"""
+__mod_name__ = "Youtube💻"
