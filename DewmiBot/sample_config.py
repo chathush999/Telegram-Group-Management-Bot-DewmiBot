ed3d879bbac9576d5a4b93d4d3b823b95707799b
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-28 02:14:32 -0700

    Update sample_config.py

--- DewmiBot/sample_config.py
+++ DewmiBot/sample_config.py
@@ -68,8 +68,8 @@ class Config(object):
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
-
-
+    REM_BG_API_KEY = get_str_key("REM_BG_API_KEY", "HazcLBC6GBs8qJsBpq7d5NUn")
+    
 class Production(Config):
     LOGGER = True
 
