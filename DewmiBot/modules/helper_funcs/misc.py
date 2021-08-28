7edfec9b98a973440fe25bf94c13a46a07da6afd
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-28 01:06:59 -0700

    Update misc.py

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🔘 Main menu 🔘", callback_data="aboutmanu_"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("⬅️Back ", callback_data="{}_prev({})".format(prefix, modulo_page))),
+                EqInlineKeyboardButton("Start menu ", callback_data="aboutmanu_"),
+             EqInlineKeyboardButton("Next➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🔘 Main menu 🔘", callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("Start menu", callback_data="aboutmanu_")]]
 
     return pairs
 
