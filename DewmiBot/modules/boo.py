d9461a99ef2eebc708a76ae4f1c8e513124568d7
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-29 21:21:01 -0700

    gg

--- DewmiBot/modules/boo.py
+++ DewmiBot/modules/boo.py
@@ -1,8 +1,6 @@
 __help__ = """
 @szrosebot🇱🇰
 ** book  **
- ❍ /sbook  <book name > : Search any book useing this bot 
- 
- 
+ ❍ /sbook  <book name > : Search any book useing this bot  
 """
-__mod_name__ = "Books📕 "
+__mod_name__ = "Books "
