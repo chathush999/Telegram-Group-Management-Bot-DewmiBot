f29906db6a55f9e9e887381995a98a39ca7cce94
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-29 22:07:55 -0700

    Update __init__.py

--- DewmiBot/__init__.py
+++ DewmiBot/__init__.py
@@ -93,8 +93,6 @@ if ENV:
     IBM_WATSON_CRED_URL = os.environ.get("IBM_WATSON_CRED_URL", None)
     IBM_WATSON_CRED_PASSWORD = os.environ.get("IBM_WATSON_CRED_PASSWORD", None)
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    BOT_ID = os.environ.get("BOT_ID","1942455194")
-    MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
     
     try:
         WHITELIST_CHATS = set(
