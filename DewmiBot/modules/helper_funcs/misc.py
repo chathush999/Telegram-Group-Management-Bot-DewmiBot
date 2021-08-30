fd77f20ff23d534612d78a7a6d63dea84a7173bd
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-29 22:30:11 -0700

    Update misc.py

--- DewmiBot/modules/helper_funcs/misc.py
+++ DewmiBot/modules/helper_funcs/misc.py
@@ -1,3 +1,4 @@
+  
 from math import ceil
 from typing import Dict, List
 
@@ -57,21 +58,21 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     calc = len(modules) - round(round_num)
     if calc == 1:
         pairs.append((modules[-1], ))
-    elif calc == 3:
+    elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) /8)
+    max_num_pages = ceil(len(pairs) / 10)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("Back ", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("close", callback_data="aboutmanu_"),
+            (EqInlineKeyboardButton("Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Close", callback_data="aboutmanu_"),
              EqInlineKeyboardButton("Next", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("close", callback_data="aboutmanu_")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="aboutmanu_")]]
 
     return pairs
 
