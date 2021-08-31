75d3e3ced76920520e1c7a64c8a083ab3d4f494a
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-31 02:48:16 -0700

    Update covid.py

--- DewmiBot/modules/covid.py
+++ DewmiBot/modules/covid.py
@@ -12,11 +12,11 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Global Results** 🦠\nConfirmed🌡: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths⚰️: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered♻️: {r['recovered']:,}\nActive🩸: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Results for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths⚰️: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered♻️: {r['recovered']:,}\nActive🩸: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
